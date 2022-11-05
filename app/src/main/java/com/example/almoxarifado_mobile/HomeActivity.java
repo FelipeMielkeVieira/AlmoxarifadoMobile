239bd32d379501eda3f4bf654c729c40f5fe9916
Felipe Vieira <felipemielkevieira2@gmail.com>
2022-11-04 21:45:22 -0300

    04/11 - 21:45

---
+++ app/src/main/java/com/example/almoxarifado_mobile/HomeActivity.java
@@ -0,0 +1,34 @@
+package com.example.almoxarifado_mobile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.almoxarifado_mobile.utils.ProdutoAdapter;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private ProdutoAdapter adapter;
+    String[] lista = {"aaaaaa", "bbbbbbb", "ccccccccccc", "dddddddddddddd", "eeeeeeeeeeee", "ffffffffffff", "ggggggggggg"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        setRecyclerView();
+    }
+
+    private void setRecyclerView() {
+        recyclerView = (RecyclerView) findViewById(R.id.lista);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new ProdutoAdapter(lista);
+        recyclerView.setAdapter(adapter);
+    }
+}
